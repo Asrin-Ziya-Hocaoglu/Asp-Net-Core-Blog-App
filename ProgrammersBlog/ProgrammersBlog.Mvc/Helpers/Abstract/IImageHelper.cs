9385e144beb9aaad44ff27b1a2c0d73615d4a7da
Asrin Ziya Hocaoglu <asrinziyahocaoglu19@gmail.com>
2022-06-17 16:11:42 +0300

    Blog App
    Just Admin Dashboard is completed

---
+++ ProgrammersBlog/ProgrammersBlog.Mvc/Helpers/Abstract/IImageHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+
+namespace ProgrammersBlog.Mvc.Helpers.Abstract
+{
+    public interface IImageHelper
+    {
+        Task<IDataResult<ImageUploadedDto>>
+            UploadUserImage(string userName, IFormFile pictureFile, string folderName="userImages");
+
+        IDataResult<ImageDeletedDto> Delete(string pictureName);
+    }
+}
---
+++ ProgrammersBlog/ProgrammersBlog.Services/Concrete/CommentManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using ProgrammersBlog.Shared.Utilities.Results.Concrete;
+
+namespace ProgrammersBlog.Services.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CommentManager(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IDataResult<int>> CountAsync()
+        {
+            var commentCount = await _unitOfWork.Comments.CountAsync();
+            if (commentCount>-1)
+            {
+                return new DataResult<int>(ResultStatus.Success, commentCount);
+            }
+            else
+            {
+                return new DataResult<int>(ResultStatus.Error, $"Beklenmedik bir hata ile karşılaşıldı", -1);
+            }
+        }
+
+        public async Task<IDataResult<int>> CountByNonDeletedAsync()
+        {
+            var commentCount = await _unitOfWork.Comments.CountAsync(c=>!c.IsDeleted);
+            if (commentCount > -1)
+            {
+                return new DataResult<int>(ResultStatus.Success, commentCount);
+            }
+            else
+            {
+                return new DataResult<int>(ResultStatus.Error, $"Beklenmedik bir hata ile karşılaşıldı", -1);
+            }
+        }
+    }
+}
